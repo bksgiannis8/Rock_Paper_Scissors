af3f713170633ba7230e5f2b4590334adb8cc9e8
giannis <bkgiannis8@gmail.com>
2022-09-29 03:11:15 +0300

    Make changes to addEventListener

--- interactions.js
+++ interactions.js
@@ -1,3 +1,11 @@
-const btn = document.querySelectorAll("#btn").forEach(item => {
-    item.addEventListener('click', playRound(item.id));   
-});
+function getComputerChoice() {
+    const rockPaperScissors = ["Rock", "Paper", "Scissors"];
+    let pcChoice = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
+    return pcChoice
+}
+
+const btn = document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+    }
+}
